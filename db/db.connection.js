943fba53d1ed52cd0327adb9294d320048ce2236
Dushyant Pratap Singh <singhdushyant2901@gmail.com>
2024-05-19 02:47:18 +0530

    Initial commit

---
+++ db/db.connection.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+const mongoURI = process.env.MONGODB;
+
+mongoose
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connect to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB: ", error);
+  });
